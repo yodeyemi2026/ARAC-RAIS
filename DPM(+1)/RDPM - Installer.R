f4d4a464ae6cce98cdcb83d95eaa3417ca8b98ec
yodeyemi2026 <yodeyemi@aracscience.com>
2026-07-12 14:14:52 -0700

    Delete DPM(+1)/RDPM - Installer.R

--- DPM(+1)/RDPM - Installer.R
+++
@@ -1,9 +0,0 @@
-#RDPM.R Required package installer
-#Version 1.7
-#Revised: Feb-13-2018
-
-#List of packages
-pk<-c('visualize','tmvtnorm','truncnorm','readxl','openxlsx','plyr','abind',
-  'diagram','ggplot2','ff','data.table','DBI','odbc')
-install.packages(pk)
-
